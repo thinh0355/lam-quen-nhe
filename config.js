b2a636ec81e0f624f1068e9ae34ab3f77f2bf572
thinh0355 <86761264+thinh0355@users.noreply.github.com>
2021-07-01 16:06:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,16 +4,16 @@ const CONFIG = {
     Anh như con thuyền nhỏ 
     Chưa 1 lần ra khơi
     Nói chuyện tiếp chứ',
-    btnIntro: 'bắt đầu nào',
+    btnIntro: 'Bắt đầu nào',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'đồng ý choa làm quenn với e nhá <33',
+    btnYes: 'Đồng ý cho a làm quen với e nhá <33',
     btnNo: 'Không, đâu a  à :3',
     question:'Trên thế giới hơn 7 tỉ người liệu e có muốn nói chuyện cùng anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'đồng ý luốn nhé <33333333',
+    reply: 'Đồng ý luốn nhé <33333333',
     mess: 'Anh biết mà 🥰. thank nhiều nhiều 😘😘',
-    messDesc: 'ib luôn nhé.',
+    messDesc: 'Ib luôn nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //https://www.facebook.com/thinh.thinh02/
 }
