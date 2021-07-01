33fadf714e3815135e7f4672a1eafe301924072e
thinh0355 <86761264+thinh0355@users.noreply.github.com>
2021-07-01 16:03:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
     introTitle: 'hello bạn nhé!',
-    introDesc: `tình yêu của em là biển rộng
-    anh như con thuyền nhỏ 
-    chưa 1 lần ra khơi
+    introDesc: `Tình yêu của em là biển rộng
+    Anh như con thuyền nhỏ 
+    Chưa 1 lần ra khơi
     Nói chuyện tiếp chứ',
     btnIntro: 'bắt đầu nào',
-    title: ' Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: ' Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'đồng ý choa làm quenn với e nhá <33',
     btnNo: 'Không, đâu a  à :3',
     question:'Trên thế giới hơn 7 tỉ người liệu e có muốn nói chuyện cùng anh <3',
