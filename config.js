67f678d93b89cd4c23e3a76a3775328e2ccd6ddb
thinh0355 <86761264+thinh0355@users.noreply.github.com>
2021-07-01 12:01:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'hello chị em là thịnh đây !',
+    introTitle: 'hello chị em là thịnh đây!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
