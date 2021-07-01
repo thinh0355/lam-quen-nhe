799b52d52be19d6dd9b0da7bf3df057f330465a3
thinh0355 <86761264+thinh0355@users.noreply.github.com>
2021-07-01 15:55:03 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,8 +5,8 @@ const CONFIG = {
     chưa 1 lần ra khơi
     Nói chuyện tiếp chứ',
     btnIntro: 'bắt đầu nào',
-    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
+    title: ' Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: ' Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'đồng ý choa làm quenn với e nhá <33',
     btnNo: 'Không, đâu a  à :3',
     question:'Trên thế giới hơn 7 tỉ người liệu e có muốn nói chuyện cùng anh <3',
